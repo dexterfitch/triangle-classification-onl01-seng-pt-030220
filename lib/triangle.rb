8972dfd0a84cb4a9eb4bc39f414a7739bf55b39c
Dexter Fitch <fitch.dexter@gmail.com>
2020-03-31 22:23:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :sideA, :sideB, :sideC
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+
+  def valid?
+    if @sideA > 0 && @sideB > 0 && @sideC > 0
+      if @sideA + @sideB > @sideC && @sideA + @sideC > @sideB && @sideB + @sideC > @sideA
+        true
+      else
+        false
+      end
+    else
+      false
+    end
+  end
+
+  def kind
+    if @sideA == @sideB && @sideB == @sideA
+      :equilateral
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "not a valid triangle"
+    end
+  end
 end
